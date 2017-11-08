012005d79f83ad7548a117cb2be1114c48d19ef5
Samrey Steven <33366679+fluxo7@users.noreply.github.com>
2017-11-09 05:03:35 +0800

    Update menu.sh

--- method3/menu.sh
+++ method3/menu.sh
@@ -18,32 +18,6 @@ echo
 	#if [ "$MYIP" = "" ]; then
 		#MYIP=$(wget -qO- ipv4.icanhazip.com)
 	#fi
-
-	clear
-
-	#echo "--------------- Selamat datang di Server - IP: $MYIP ---------------"
-	echo ""
-	echo ""
-	echo ""
-	echo ""
-	echo ""
-	echo ""
-	echo ""
-	echo ""
-	echo ""
-	#cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
-	#cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
-	#freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
-	#tram=$( free -m | awk 'NR==2 {print $2}' )
-	#swap=$( free -m | awk 'NR==4 {print $2}' )
-	#up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
-
-	#echo -e "\e[032;1mCPU model:\e[0m $cname"
-	#echo -e "\e[032;1mNumber of cores:\e[0m $cores"
-	#echo -e "\e[032;1mCPU frequency:\e[0m $freq MHz"
-	#echo -e "\e[032;1mTotal amount of ram:\e[0m $tram MB"
-	#echo -e "\e[032;1mTotal amount of swap:\e[0m $swap MB"
-	#echo -e "\e[032;1mSystem uptime:\e[0m $up"
 lolcat -F 0.3 -S 0 /usr/bin/bannermenu
 echo "                          Server: $MYIP" | lolcat
 date +"                          %A, %d-%m-%Y" | lolcat
